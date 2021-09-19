0b84fb9152e2bcc4261e0616351b528afcdd2087
nivcoo <nico64800@live.fr>
2021-09-19 13:53:15 +0200

    improv. remove warnings

--- src/main/java/fr/nivcoo/goldencooldown/GoldenCooldown.java
+++ src/main/java/fr/nivcoo/goldencooldown/GoldenCooldown.java
@@ -1,52 +1,51 @@
 /**
- * 
+ *
  */
 package fr.nivcoo.goldencooldown;
 
-import java.io.File;
-
+import fr.nivcoo.goldencooldown.events.PlayerItemConsume;
+import fr.nivcoo.goldencooldown.utils.Config;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import fr.nivcoo.goldencooldown.events.PlayerItemConsume;
-import fr.nivcoo.goldencooldown.utils.Config;
+import java.io.File;
 
 public class GoldenCooldown extends JavaPlugin {
-	private static GoldenCooldown INSTANCE;
-	private Config config;
+    private static GoldenCooldown INSTANCE;
+    private Config config;
 
-	@Override
-	public void onEnable() {
-		INSTANCE = this;
-		saveDefaultConfig();
-		config = new Config(new File("plugins" + File.separator + "GoldenCooldown" + File.separator + "config.yml"));
-		getCommand("gc").setExecutor(new GoldenCooldownCommand());
-		Bukkit.getPluginManager().registerEvents(new PlayerItemConsume(), this);
-		Bukkit.getConsoleSender().sendMessage("§c==============§b===============");
-		Bukkit.getConsoleSender().sendMessage("§7GoldenCooldown §av" + this.getDescription().getVersion());
-		Bukkit.getConsoleSender().sendMessage("§aPlugin Enabled !");
-		Bukkit.getConsoleSender().sendMessage("§c==============§b===============");
-	}
+    @Override
+    public void onEnable() {
+        INSTANCE = this;
+        saveDefaultConfig();
+        config = new Config(new File("plugins" + File.separator + "GoldenCooldown" + File.separator + "config.yml"));
+        getCommand("gc").setExecutor(new GoldenCooldownCommand());
+        Bukkit.getPluginManager().registerEvents(new PlayerItemConsume(), this);
+        Bukkit.getConsoleSender().sendMessage("§c==============§b===============");
+        Bukkit.getConsoleSender().sendMessage("§7GoldenCooldown §av" + this.getDescription().getVersion());
+        Bukkit.getConsoleSender().sendMessage("§aPlugin Enabled !");
+        Bukkit.getConsoleSender().sendMessage("§c==============§b===============");
+    }
 
-	@Override
-	public void onDisable() {
-		Bukkit.getConsoleSender().sendMessage("§c==============§b===============");
-		Bukkit.getConsoleSender().sendMessage("§7GoldenCooldown §cv" + this.getDescription().getVersion());
-		Bukkit.getConsoleSender().sendMessage("§cPlugin Disabled !");
-		Bukkit.getConsoleSender().sendMessage("§c==============§b===============");
-	}
-	
-	
-	public void reload() {
-		config.loadConfig();
-	}
+    @Override
+    public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage("§c==============§b===============");
+        Bukkit.getConsoleSender().sendMessage("§7GoldenCooldown §cv" + this.getDescription().getVersion());
+        Bukkit.getConsoleSender().sendMessage("§cPlugin Disabled !");
+        Bukkit.getConsoleSender().sendMessage("§c==============§b===============");
+    }
 
 
-	public static GoldenCooldown get() {
-		return INSTANCE;
-	}
-	
-	public Config getConfiguration() {
-		return config;
-	}
+    public void reload() {
+        config.loadConfig();
+    }
+
+
+    public static GoldenCooldown get() {
+        return INSTANCE;
+    }
+
+    public Config getConfiguration() {
+        return config;
+    }
 }
--- src/main/java/fr/nivcoo/goldencooldown/utils/Config.java
+++ src/main/java/fr/nivcoo/goldencooldown/utils/Config.java
@@ -1,10 +1,5 @@
 package fr.nivcoo.goldencooldown.utils;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.configuration.ConfigurationSection;
@@ -12,125 +7,130 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Config {
-	private File fichierConfig;
-	private FileConfiguration fconfig;
+    private File fichierConfig;
+    private FileConfiguration fconfig;
 
-	public Config(File file) {
-		this.fichierConfig = file;
-		loadConfig();
-	}
+    public Config(File file) {
+        this.fichierConfig = file;
+        loadConfig();
+    }
 
-	public void save() {
-		try {
-			fconfig.save(fichierConfig);
-		} catch (IOException ex) {
-			Bukkit.getLogger().severe("An error has occured while saving file " + fichierConfig.getPath());
-		}
-	}
+    public void save() {
+        try {
+            fconfig.save(fichierConfig);
+        } catch (IOException ex) {
+            Bukkit.getLogger().severe("An error has occured while saving file " + fichierConfig.getPath());
+        }
+    }
 
-	public void loadConfig() {
-		fconfig = YamlConfiguration.loadConfiguration(fichierConfig);
-	}
+    public void loadConfig() {
+        fconfig = YamlConfiguration.loadConfiguration(fichierConfig);
+    }
 
-	public void set(String path, Object obj) {
-		if (obj instanceof Location) {
-			Location loc = (Location) obj;
-			fconfig.set(path + ".x", loc.getX());
-			fconfig.set(path + ".y", loc.getY());
-			fconfig.set(path + ".z", loc.getZ());
-			fconfig.set(path + ".yaw", loc.getYaw());
-			fconfig.set(path + ".pitch", loc.getPitch());
-			fconfig.set(path + ".world", loc.getWorld().getName());
-		} else
-			fconfig.set(path, obj);
-		save();
-	}
+    public void set(String path, Object obj) {
+        if (obj instanceof Location) {
+            Location loc = (Location) obj;
+            fconfig.set(path + ".x", loc.getX());
+            fconfig.set(path + ".y", loc.getY());
+            fconfig.set(path + ".z", loc.getZ());
+            fconfig.set(path + ".yaw", loc.getYaw());
+            fconfig.set(path + ".pitch", loc.getPitch());
+            fconfig.set(path + ".world", loc.getWorld().getName());
+        } else
+            fconfig.set(path, obj);
+        save();
+    }
 
-	public String getString(String path) {
-		return getString(path, new Object[] {});
-	}
+    public String getString(String path) {
+        return getString(path, new Object[]{});
+    }
 
-	public String getString(String path, Object... lists) {
-		String name = fconfig.getString(path);
-		if (name != null && lists != null)
-			for (int i = 0; i < lists.length; i++)
-				name = name.replace("{" + i + "}", lists[i].toString());
+    public String getString(String path, Object... lists) {
+        String name = fconfig.getString(path);
+        if (name != null && lists != null)
+            for (int i = 0; i < lists.length; i++)
+                name = name.replace("{" + i + "}", lists[i].toString());
 
-		return name == null ? null : name.replace("&", "§");
-	}
+        return name == null ? null : name.replace("&", "§");
+    }
 
-	public int getInt(String path) {
-		return fconfig.getInt(path);
-	}
+    public int getInt(String path) {
+        return fconfig.getInt(path);
+    }
 
-	public long getLong(String path) {
-		return fconfig.getLong(path);
-	}
+    public long getLong(String path) {
+        return fconfig.getLong(path);
+    }
 
-	public boolean getBoolean(String path) {
-		return fconfig.getBoolean(path);
-	}
+    public boolean getBoolean(String path) {
+        return fconfig.getBoolean(path);
+    }
 
-	public double getDouble(String path) {
-		return fconfig.getDouble(path);
-	}
+    public double getDouble(String path) {
+        return fconfig.getDouble(path);
+    }
 
-	public List<String> getStringList(String path) {
-		List<String> name = new ArrayList<>();
-		for (String nom : fconfig.getStringList(path))
-			name.add(nom.replace("&", "§"));
-		return name;
-	}
+    public List<String> getStringList(String path) {
+        List<String> name = new ArrayList<>();
+        for (String nom : fconfig.getStringList(path))
+            name.add(nom.replace("&", "§"));
+        return name;
+    }
 
-	public List<Integer> getIntegerList(String path) {
-		List<Integer> name = new ArrayList<>();
-		for (Integer nom : fconfig.getIntegerList(path))
-			name.add(nom);
-		return name;
-	}
+    public List<Integer> getIntegerList(String path) {
+        List<Integer> name = new ArrayList<>();
+        for (Integer nom : fconfig.getIntegerList(path))
+            name.add(nom);
+        return name;
+    }
 
-	public List<String> getKeys(String path) {
-		List<String> list = new ArrayList<>();
-		if ("".equalsIgnoreCase(path)) {
-			for (String section : fconfig.getKeys(false))
-				list.add(section);
-		} else {
-			ConfigurationSection cs = fconfig.getConfigurationSection(path);
-			if (cs == null)
-				return list;
-			for (String section : cs.getKeys(false))
-				list.add(section);
-		}
-		return list;
-	}
+    public List<String> getKeys(String path) {
+        List<String> list = new ArrayList<>();
+        if ("".equalsIgnoreCase(path)) {
+            for (String section : fconfig.getKeys(false))
+                list.add(section);
+        } else {
+            ConfigurationSection cs = fconfig.getConfigurationSection(path);
+            if (cs == null)
+                return list;
+            for (String section : cs.getKeys(false))
+                list.add(section);
+        }
+        return list;
+    }
 
-	public ItemStack getItem(String path) {
-		return fconfig.getItemStack(path);
-	}
+    public ItemStack getItem(String path) {
+        return fconfig.getItemStack(path);
+    }
 
-	public Location getLocation(String path) {
-		double x = fconfig.getDouble(path + ".x");
-		double y = fconfig.getDouble(path + ".y");
-		double z = fconfig.getDouble(path + ".z");
-		float yaw = (float) fconfig.getDouble(path + ".yaw");
-		float pitch = (float) fconfig.getDouble(path + ".pitch");
-		String world = fconfig.getString(path + ".world");
-		if (world == null || "".equalsIgnoreCase(world))
-			return null;
-		return new Location(Bukkit.getWorld(world), x, y, z, yaw, pitch);
-	}
-	
+    public Location getLocation(String path) {
+        double x = fconfig.getDouble(path + ".x");
+        double y = fconfig.getDouble(path + ".y");
+        double z = fconfig.getDouble(path + ".z");
+        float yaw = (float) fconfig.getDouble(path + ".yaw");
+        float pitch = (float) fconfig.getDouble(path + ".pitch");
+        String world = fconfig.getString(path + ".world");
+        if (world == null || "".equalsIgnoreCase(world))
+            return null;
+        return new Location(Bukkit.getWorld(world), x, y, z, yaw, pitch);
+    }
 
-	public boolean exist(String path) {
-		return fconfig.contains(path);
-	}
 
-	public void reload() {
-		loadConfig();
-	}
+    public boolean exist(String path) {
+        return fconfig.contains(path);
+    }
 
-	public File getFile() {
-		return fichierConfig;
-	}
+    public void reload() {
+        loadConfig();
+    }
+
+    public File getFile() {
+        return fichierConfig;
+    }
 }
